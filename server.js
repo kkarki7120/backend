07e17bd37b96ea96943c4af01bad0693b9f24a11
kkarki7120 <kkarki7120@gmail.com>
2024-07-17 14:24:09 +0545

    newcommit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: '',
+  host: '172.31.36.44',
   user: 'reactuser', // Replace with your MySQL username
   password: '12345', // Replace with your MySQL password
   database: 'react',
